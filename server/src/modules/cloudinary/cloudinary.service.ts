a53a9082d73dfbc8bbfd569436b8a3a9162b663e
quocsi014 <quocsi014@gmail.com>
2024-12-28 23:40:03 +0700

    fix cloudinary service

--- server/src/modules/cloudinary/cloudinary.service.ts
+++ server/src/modules/cloudinary/cloudinary.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { v2 as cloudinary } from 'cloudinary';
-import { ImageDTO } from 'src/modules/image/dto/image.dto';
+export type UploadFileResponse = {
+  url: string;
+  key: string;
+};
 @Injectable()
 export class CloudinaryService {
   constructor(private configService: ConfigService) {
@@ -11,18 +14,44 @@ export class CloudinaryService {
       api_secret: this.configService.get<string>('cloudinary.api_secret'),
     });
   }
-  async uploadImage(file: Express.Multer.File): Promise<ImageDTO> {
-    const result = await cloudinary.uploader.upload(file.path, {
-      folder: 'blog/avatar',
-    });
-    const image: ImageDTO = {
-      url: result.secure_url,
-      key: result.public_id,
-    };
-    return image;
+  // async uploadImage(file: Express.Multer.File): Promise<ImageDTO> {
+  //   const result = await cloudinary.uploader.upload(file.path, {
+  //     folder: 'blog/avatar',
+  //   });
+  //   const image: ImageDTO = {
+  //     url: result.secure_url,
+  //     key: result.public_id,
+  //   };
+  //   return image;
+  // }
+  //
+  // async deleteImage(key: string): Promise<void> {
+  //   await cloudinary.uploader.destroy(key);
+  // }
+  private readonly logger = new Logger(CloudinaryService.name);
+  async uploadImage(
+    file: Express.Multer.File,
+    folderName: string,
+  ): Promise<UploadFileResponse> {
+    try {
+      const result = await cloudinary.uploader.upload(file.path, {
+        folder: folderName,
+      });
+      const res: UploadFileResponse = {
+        url: result.secure_url,
+        key: result.public_id,
+      };
+      return res;
+    } catch (error) {
+      this.logger.error(`Upload file error: ${error.message}`);
+      throw new InternalServerErrorException('Some thing went wrong');
+    }
   }
-
   async deleteImage(key: string): Promise<void> {
-    await cloudinary.uploader.destroy(key);
+    try {
+      await cloudinary.uploader.destroy(key);
+    } catch (error) {
+      this.logger.error(`Delete file error: ${error.message}`);
+    }
   }
 }
